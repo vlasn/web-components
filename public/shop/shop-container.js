59681c249aca36f7935eb0108969bbd9f627d8ba
Aleksandr Beliaev <aleksandr.beliaev@nortal.com>
2018-09-05 18:57:41 +0300

    Added styles for the shop container to a constant

--- public/shop/shop-container.js
+++ public/shop/shop-container.js
@@ -15,18 +15,17 @@ class ShopContainer extends HTMLElement {
     async connectedCallback() {
 
         const items = await this.getItems()
+        const style = `#shopping-cart {
+              position:fixed; 
+              top:20px; 
+              right:20px; 
+              box-shadow: 0 0 3px #ccc;
+          }`
         console.log('shop items', items)
         this.shadow.innerHTML =  `<section id="shopping-cart">
                                     ${items.map(item => `<shop-row id=${item.id} name=${item.name} cost=${item.cost} ></shop-row>`).join("")}
                                   </section>
-                                  <style>
-                                      #shopping-cart {
-                                          position:fixed; 
-                                          top:20px; 
-                                          right:20px; 
-                                          box-shadow: 0 0 3px #ccc;
-                                      }
-                                  </style>`;
+                                  <style>${style}</style>`;
                                   
     }
 }
