5a1899e9ec5c7ef183745d26f14e3c697e754747
tonis2 <you@example.com>
2018-09-05 19:02:35 +0300

    update cart

--- public/shop/shop-container.js
+++ public/shop/shop-container.js
@@ -6,24 +6,20 @@ class ShopContainer extends HTMLElement {
     }
 
     async connectedCallback() {
-<<<<<<< HEAD
-        console.log(await game.getShopInventory())
-=======
 
         const items = await this.getItems()
         const style = `#shopping-cart {
-              position:fixed; 
-              top:20px; 
-              right:20px; 
+              position:fixed;
+              top:20px;
+              right:20px;
               box-shadow: 0 0 3px #ccc;
           }`
->>>>>>> d3d652b70a67f9952147435966da7ce53b228b00
         console.log('shop items', items)
         this.shadow.innerHTML =  `<section id="shopping-cart">
                                     ${items.map(item => `<shop-row id=${item.id} name=${item.name} cost=${item.cost} ></shop-row>`).join("")}
                                   </section>
                                   <style>${style}</style>`;
-                                  
+
     }
 }
 
