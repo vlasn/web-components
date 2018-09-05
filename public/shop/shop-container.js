2613293e021e7fbb7254148c005bcfe3236a90c1
tonis2 <you@example.com>
2018-09-05 19:15:41 +0300

    update shop-cart

--- public/shop/shop-container.js
+++ public/shop/shop-container.js
@@ -1,28 +1,28 @@
-
 class ShopContainer extends HTMLElement {
-    constructor() {
-        super()
-        this.shadow = this.attachShadow({mode: 'open'})
-    }
+  constructor() {
+    super()
+    this.shadow = this.attachShadow({
+      mode: 'open'
+    })
+  }
 
-    async connectedCallback() {
-        await game.start();
+  async connectedCallback() {
+    await game.start();
+    const items = await game.getShopInventory()
 
-        const items = await game.getShopInventory()
-
-        const style = `#shopping-cart {
+    const style = `#shopping-cart {
               position:fixed;
               top:20px;
               right:20px;
               box-shadow: 0 0 3px #ccc;
           }`
-        console.log('shop items', items)
-        this.shadow.innerHTML =  `<section id="shopping-cart">
-                                    ${items.map(item => `<shop-row id=${item.id} name=${item.name} cost=${item.cost} ></shop-row>`).join("")}
-                                  </section>
-                                  <style>${style}</style>`;
+    console.log('shop items', items)
+    this.shadow.innerHTML = `<section id="shopping-cart">
+                                ${items.map(item => `<shop-row id=${item.id} name=${item.name} cost=${item.cost} ></shop-row>`).join("")}
+                              </section>
+                              <style>${style}</style>`;
 
-    }
+  }
 }
 
 
