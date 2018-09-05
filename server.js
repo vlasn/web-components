2248783a036f1348d6c3017a2bf544517c9d2ef4
vlasn <needikola@gmail.com>
2018-09-05 17:58:22 +0300

    Fixed server?!

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const { PORT = 3333 } = process.env
 app.use(express.static(path.resolve('public')))
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './src/index.html'))
+    res.sendFile(path.resolve(__dirname, './public/index.html'))
 })
 
 app.listen(PORT, () => {
