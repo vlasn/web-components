14b2a1d84c648fd73526c2ed019f71d81d2c8c4d
tonis2 <you@example.com>
2018-09-05 18:56:10 +0300

    Merge branch 'master' of github.com:vlasn/web-components

--- public/shop/shop-container.js
+++ public/shop/shop-container.js
@@ -2,19 +2,11 @@
 class ShopContainer extends HTMLElement {
     constructor() {
         super()
-        this.game = new Game();
         this.shadow = this.attachShadow({mode: 'open'})
-
-    }
-
-    async getItems() {
-        await this.game.start();
-        return await this.game.getShopInventory();
     }
 
     async connectedCallback() {
-
-        const items = await this.getItems()
+        console.log(await game.getShopInventory())
         console.log('shop items', items)
         this.shadow.innerHTML =  `<section id="shopping-cart">
                                     ${items.map(item => `<shop-row id=${item.id} name=${item.name} cost=${item.cost} ></shop-row>`).join("")}
